5dc31eb41a06d26375e44a54bd49b4fad333aa39
Yuri Ppo <yuri.prata12@gmail.com>
2022-01-14 13:10:36 -0800

    New commit

---
+++ P12- CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto P11 - Calcula Poupança2");
+
+        double valorInvestido = 1000;
+
+        double fatorDeRendimento = 1.0036;
+
+        for (int contadorAno = 1;contadorAno <= 5; contadorAno++)
+        {
+            for (int contadorMes = 1; contadorMes <=12; contadorMes++)
+            {
+                valorInvestido = valorInvestido * fatorDeRendimento;
+            }
+
+            fatorDeRendimento += 0.0010;
+        }
+
+        Console.WriteLine("Ao término do investimento você terá R$ " + valorInvestido );
+
+        Console.WriteLine("A execução acabou. Tecle enter para sair...");
+
+        Console.ReadLine();
+
+    }
+
+}
+
